4e2e58a02c3e2df049207f3f0140a21db502c9cd
harispujiantoro09@gmail.com <harispujiantoro09@gmail.com>
2021-02-22 19:49:02 +0700

    mengubah file

--- latihan/variabel.py
+++ latihan/variabel.py
@@ -1,3 +1,30 @@
-a = 1
-b = 2
-print(a + b)
+# variabel adalah untuk menyimpan  sebuah nilai data
+
+"""
+x = 5
+y = 2
+print(x+y)
+
+# varibel casting
+x = "y"     - ini variabel string
+y = 1       - ini variabel int
+a = 1,3     - ini variabel float
+untuk melihat typenya
+print(type(x,y,a))
+
+# variabel case sensitive adalah peka terhadap huruf besar dan kecil
+x = 20
+X = "ya"
+
+# variabel tidak boleh asal harus menggunkan string dan tidak boleh diawali dgn spaci
+
+# multi word variabel name
+- camel case
+myVariabel = "haris"
+- pascal case
+MyVariabel = "haris"
+- snack case
+my_variabel = "haris"
+
+
+"""
