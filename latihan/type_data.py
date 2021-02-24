ee5af41c9cbcd1b4d65ac8bc05f0ee264689ed95
harispujiantoro09@gmail.com <harispujiantoro09@gmail.com>
2021-02-25 00:42:42 +0700

    type data

--- latihan/type_data.py
+++ latihan/type_data.py
@@ -1,9 +1,41 @@
-<<<<<<< HEAD
-#type data
-=======
+""" 
+#ada beberapa contoh type data
+- type text     = str
+- numeric type  = int, float, complex
+- squence type  = list, tuple, range
+- mapping type  = dict
+- set type      = set, frozenset
+- bolean type   = bool
+- binary type   = bytes, bytearray, memoryview
 
-print("dobleh")
+# contoh
+- x = "haris"           str
+- x = 1                 int
+- x = 12.3              float
+- x = 2k                complex
+- x = ["haris","dika"]  list
+- x = ("haris","dika")  tuple
+- x = range(2)          range
+- x = {"nama":"haris","umur":"18"}  dict
+- x = {"haris","dika"}  set
+- x = frozenset({"haris","dika"})   forozenset
+- x = true              bool
+- x = b"hello"          bytes
+- x = bytearray(4)      bytearray
+- x = memoryview(bytes(6))  memeoryview
 
-print("haris")
+# fungsi" pada string
+- pajang string
+x = "haris, ganteng"
+print(len(x))
 
->>>>>>> a87814279cdc95c18a71d028b95c8a58b237e961
+# modifikasi str
+- huruf besar = upper()
+x = "haris"
+print(x.upper())
+- huruf kecil = lower()
+x = "HARIS"
+print(x.lower())
+-
+
+"""
