575f1e06d6b79ce3e04d4ba049aee53f6f0168af
harispujiantoro09@gmail.com <harispujiantoro09@gmail.com>
2021-03-02 16:53:42 +0700

    perubahan

--- latihan/if elif else.py
+++ latihan/if elif else.py
@@ -1,17 +1,18 @@
-ipk = 0
-counter = 0
-n = float(input("masukan batas matkul: "))
+uas = float(input("masukan nilai uas: "))
+uts = float(input("masukan nilai uts: "))
+tgs = float(input("masukan nilai tugas: "))
+hdr = float(input("masukan nilai hadir: "))
 
-while counter <= n-1:
-    nilai = float(input("Nilai makul ke - {} :".format(counter)))
-    if nilai >= 85:
-        ipk += 4
-    elif nilai >= 70 and nilai < 85:
-        ipk += 3
-    elif niali >= 60 and nilai < 70:
-        ipk += 2
-    elif nilai >= 50 and nilai < 60:
-        ipk = ipk+1
-    counter = counter+1
+na = (uas * 40)+(uts * 30)+(tgs * 20)+(hdr * 10)
 
-print("ipk :", ipk/counter)
+if na >= 80:
+    index = "A"
+elif na >= 70:
+    index = "B"
+elif na >= 60:
+    index = "C"
+else:
+    index = "tidak lulus"
+
+print("nilai akhir anda: ",na)
+print("nilai index anda: ",index)
---
+++ latihan/latihan.py
@@ -0,0 +1,14 @@
+a = [1,3,5,7,9]
+b = [100,300,500,700,900]
+
+# ini adalaha megabungkan list
+subdata1 = [a,b]
+# ini adalah mengiris list 
+subdata2 = a[1:3]
+# mengubah isi dalam list
+subdata3 = b[:]
+subdata3[2] = 45 
+# merubah list dalam multidimensional
+x = [a,b]
+
+print(x)
