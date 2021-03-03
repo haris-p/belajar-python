616643656968bd39831c921f0e566ab5c7d82029
harispujiantoro09@gmail.com <harispujiantoro09@gmail.com>
2021-03-04 02:26:05 +0700

    buat program kasir

--- latihan/if elif else.py
+++ latihan/if elif else.py
@@ -1,18 +1,7 @@
-uas = float(input("masukan nilai uas: "))
-uts = float(input("masukan nilai uts: "))
-tgs = float(input("masukan nilai tugas: "))
-hdr = float(input("masukan nilai hadir: "))
+cari = int(input("masukan angka: "))
+a = [1,3,5,8,9,10,13]
 
-na = (uas * 40)+(uts * 30)+(tgs * 20)+(hdr * 10)
-
-if na >= 80:
-    index = "A"
-elif na >= 70:
-    index = "B"
-elif na >= 60:
-    index = "C"
-else:
-    index = "tidak lulus"
-
-print("nilai akhir anda: ",na)
-print("nilai index anda: ",index)
+posisi = 0
+iterasi = 0
+akhir = len(a)-1
+found = False
